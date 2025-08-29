7be78091e59c8aa46731f3a767f50d5aef089ee0
Miguel Angel <lievano.miguel.3534@eam.edu.co>
2025-08-29 11:34:35 -0500

    Commit inicial

---
+++ src/main/java/com/quantumzone/QZ_Workhub/QzWorkhubApplication.java
@@ -0,0 +1,13 @@
+package com.quantumzone.QZ_Workhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QzWorkhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QzWorkhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantumzone/QZ_Workhub/QzWorkhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantumzone.QZ_Workhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QzWorkhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
