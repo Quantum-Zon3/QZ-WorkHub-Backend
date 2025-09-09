509554565efb1ab2669e17fa0270f20c9bd1a634
Miguelangelo <lievano.miguel.3534@eam.edu.co>
2025-09-09 15:49:50 -0500

    Se realizo la implentacion del crud en ReservaRepository

---
+++ src/main/java/com/quantumzone/QZ_Workhub/persistencia/repositorio/ReservaRepository.java
@@ -0,0 +1,78 @@
+package com.quantumzone.QZ_Workhub.persistencia.repositorio;
+
+import java.util.Optional;
+import java.util.List;
+
+import com.quantumzone.QZ_Workhub.persistencia.Reserva;
+import com.quantumzone.QZ_Workhub.persistencia.Sala;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+import com.quantumzone.QZ_Workhub.persistencia.entidad.Reserva;
+
+@Repository
+public class ReservaRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    //Guardar reserva
+    public Reserva save(Sala sala) {
+        entityManager.persist(sala);
+        return sala;
+    }
+
+    //Buscar todas las reservas
+    @Transactional
+    public List<Reserva> findAll() {
+        Query query = entityManager.createNativeQuery("SELECT * FROM reserva ", Reserva.class);
+        return query.getResultList();
+    }
+
+    //Busca por id un sala
+    @Transactional
+    public Optional<Reserva> findById(Integer id) {
+        Query query = entityManager.createNativeQuery("SELECT * FROM reserva WHERE id = :id", Reserva.class);
+        query.setParameter("id", id);
+        try {
+            Reserva reserva = (Reserva) query.getSingleResult();
+            return Optional.of(reserva);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    //Elimina por id una reserva
+    @Transactional
+    public boolean deleteById(Integer id) {
+        Query query = entityManager.createNativeQuery("DELETE FROM reserva WHERE id = :id");
+        query.setParameter("id", id);
+        int delete = query.executeUpdate();
+        return delete > 0;
+    }
+
+    //Actualizar por id una reserva
+    public Optional<Reserva> update(Integer id, Reserva reserva) {
+        Query query = entityManager.createNativeQuery("UPDATE reserva SET nombre = :nombre");
+        query.setParameter("nombre", reserva.getNombre());
+        int update = query.executeUpdate();
+        if (update > 0) {
+            return findById(id);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    //Buscar reservas por filtros
+    public Optional<List<Reserva>> findByFilters(String nombre) {
+        Query query = entityManager.createNativeQuery("SELECT * FROM reserva WHERE nombre LIKE :nombre", Reserva.class);
+        query.setParameter("nombre", nombre);
+        try {
+            List<Reserva> reserva = query.getResultList();
+            return Optional.of(reserva);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+}
