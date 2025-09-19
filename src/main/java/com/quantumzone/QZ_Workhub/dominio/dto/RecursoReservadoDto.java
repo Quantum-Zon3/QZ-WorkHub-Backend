a717dfe219fbf7e3575e4aaecb33fa1931a1ce6a
Miguel Angel <lievano.miguel.3534@eam.edu.co>
2025-09-19 11:04:12 -0500

    Escritura de los DTO's usuario, sala, reserva, reporte, recusoReservado

--- src/main/java/com/quantumzone/QZ_Workhub/dominio/dto/RecursoReservadoDto.java
+++ src/main/java/com/quantumzone/QZ_Workhub/dominio/dto/RecursoReservadoDto.java
@@ -1,4 +1,15 @@
 package com.quantumzone.QZ_Workhub.dominio.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "Informacion de recursos reservados")
 public class RecursoReservadoDto {
+    @Schema(description = "identificador de un recursoReservado")
+    private Long id;
 }
--- src/main/java/com/quantumzone/QZ_Workhub/dominio/dto/UsuarioDto.java
+++ src/main/java/com/quantumzone/QZ_Workhub/dominio/dto/UsuarioDto.java
@@ -1,4 +1,44 @@
 package com.quantumzone.QZ_Workhub.dominio.dto;
 
+import com.quantumzone.QZ_Workhub.dominio.enums.Rol;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Informacion del usuario")
 public class UsuarioDto {
+    @Schema(description = "Id única del usuario", example = "1")
+    private Long cedula;
+
+    @Schema(description = "Nombre del usuario", example = "Juanito", required = true, maxLength = 100)
+    private String nombre;
+
+    @Schema(description = "Apellido del usuario", example = "Ruiz", required = true, maxLength = 100)
+    private String apellido;
+
+    @Schema(description = "Email del usuario", example = "juan@gmail.com", required = true, maxLength = 100)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Schema(description = "Rol del usuario", example = "MIEMBRO", required = true, maxLength = 100)
+    private Rol rol;
+
+    @Schema(description = "Contraseña del usuario", example = "mimamamemima", required = true, maxLength = 100)
+    private String contraseña;
+
+    @Schema(description = "Fecha de registro del usuario", example = "2025-09-18T10:30:00", required = true, maxLength = 100)
+    private LocalDateTime fechaRegistro;
+
+    @Schema(description = "Telefono del usuario", example = "309 8634519", required = true, maxLength = 100)
+    private String telefono;
+
 }
