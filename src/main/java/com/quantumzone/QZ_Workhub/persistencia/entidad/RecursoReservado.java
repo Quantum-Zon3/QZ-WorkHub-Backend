f37521bd5950ecc3c9299c8d2a0044bfde5ad0d4
CarlosMPatino7822 <carlosmariopatinoramirez@gmail.com>
2025-09-26 16:56:09 -0600

    Actualizado recursoReservado con sus métodos correspondientes

--- src/main/java/com/quantumzone/QZ_Workhub/persistencia/entidad/RecursoReservado.java
+++ src/main/java/com/quantumzone/QZ_Workhub/persistencia/entidad/RecursoReservado.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,9 +15,17 @@ import lombok.NoArgsConstructor;
 @Table(name = "recurso_reservado")
 public class RecursoReservado {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // id auto generada
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// id auto generada
+    @Column(name = "id_recuso_reservado", nullable = false)
     private Long id;
-
+    @Column(name = "cantidad" , nullable = false)
+    private Long cantidad;
+    @Column(name = "fecha_inicio", nullable = false)
+    private LocalDateTime fechaInicio;
+    @Column(name = "fecha_fin", nullable = false)
+    private LocalDateTime fechaFin;
+    @Column(name = "monto_total", nullable = false)
+    private int montoTotal;
     @ManyToOne
     @JoinColumn(name = "id_recurso", nullable = false)
     private Recurso recurso;
