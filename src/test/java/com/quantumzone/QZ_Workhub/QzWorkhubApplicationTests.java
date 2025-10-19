9ddd4d09d6ba126d9e33e98e90a02fe26fc54930
Miguelangelo <lievano.miguel.3534@eam.edu.co>
2025-10-18 23:19:54 -0500

    Modificaciones en gradle, aplication-test-properties y qzAplicationTests para jacoco

--- src/test/java/com/quantumzone/QZ_Workhub/QzWorkhubApplicationTests.java
+++ src/test/java/com/quantumzone/QZ_Workhub/QzWorkhubApplicationTests.java
@@ -2,8 +2,10 @@ package com.quantumzone.QZ_Workhub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class QzWorkhubApplicationTests {
 
 	@Test
